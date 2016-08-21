b695cf718fc2acabc9584aeac8608b8f5a0d8498
Gus Bonfant <gbonfant@me.com>
2016-08-21 16:45:43 +0100

    zoopla integration

--- app/start.rb
+++ app/start.rb
@@ -1,18 +1,10 @@
 require 'rufus-scheduler'
-require_relative 'properties'
+require_relative 'notifier'
 
 scheduler = Rufus::Scheduler.new
-properties = Properties.new({
-  postcode: 'N1',
-  town: 'London',
-  listing_status: 'rent',
-  maximum_price: 575,
-  order_by: 'age',
-  radius: 1,
-  page_size: 100
-})
+notifier = Notifier.new
 
 # scheduler.cron '0 12 * * *', properties
-scheduler.in '3s', properties
+scheduler.in '3s', notifier
 
 scheduler.join
